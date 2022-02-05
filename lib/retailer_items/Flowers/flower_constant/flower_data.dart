c52a0b496c4035a02376362faded1e80a2033866
Anshuman021 <anshumanjodha21@gmail.com>
2022-02-05 14:21:47 +0530

    Updation

--- lib/retailer_items/Flowers/flower_constant/flower_data.dart
+++ lib/retailer_items/Flowers/flower_constant/flower_data.dart
@@ -7,7 +7,7 @@ List items = [
     "id": 1,
     "product": "Abutilon",
     "productdetail":
-        " Abutilon Contains: \n abutilon, (genus Abutilon),\n genus of over 100\n species of herbaceous plants \n and partly woody\n shrubs of the mallow \n family (Malvaceae) \n native to tropical\n  and warm temperate areas.\n It includes several species\n used as houseplants\n and in gardens for their\n white to deep orange,\n usually nodding,\n five-petaled blossoms.",
+        "Abutilon Contains:\nabutilon, (genus Abutilon),\ngenus of over 100\nspecies of herbaceous plants\nand partly woody\nshrubs of the mallow\nfamily (Malvaceae)\nnative to tropical\nand warm temperate areas.\nIt includes several species\nused as houseplants\nand in gardens for their\nwhite to deep orange,\nusually nodding,\nfive-petaled blossoms.",
     "img": "assets/RetailerFlowers/Abutilon.jpg",
     "p_price": 80.00,
     "p_promotion_price": 55.00
@@ -16,7 +16,7 @@ List items = [
     "id": 2,
     "product": "Acacia",
     "productdetail":
-        " Acacia Contains: \n The flowers are usually\n yellow but occasionally white\n and have many stamens apiece\n, giving each one a fuzzy \n appearance. The fruits are \n legumes and are highly \n variable in appearance,\n depending on the species. \n Acacias are often\n confused with members of \n closely related genus Mimosa.\n Wattle tree (Acacia species).",
+        "Acacia Contains:\nThe flowers are usually\nyellow but occasionally white\nand have many stamens apiece,\ngiving each one a fuzzy\nappearance. The fruits are\nlegumes and are highly\nvariable in appearance,\ndepending on the species.\nAcacias are often\nconfused with members of\nclosely related genus Mimosa.\nWattle tree (Acacia species).",
     "img": "assets/RetailerFlowers/Acacia.jpg",
     "p_price": 90.00,
     "p_promotion_price": 40.00
@@ -25,7 +25,7 @@ List items = [
     "id": 3,
     "product": "Aconite",
     "productdetail":
-        " Aconite Contains:\n Aconitum napellus,\n monk's-hood,\n aconite or wolfsbane, \nis a species \nof highly toxic flowering plant \nin the genus Aconitum of the \nfamily Ranunculaceae,\n native and endemic to\n western and central Europe.\n It is an herbaceous perennial\n plant growing to 1 m\n (3 ft 3 in) tall, with\n hairless stems and leaves.",
+        "Aconite Contains:\nAconitum napellus,\nmonk's-hood,\naconite or wolfsbane,\nis a species\nof highly toxic flowering plant\nin the genus Aconitum of the\nfamily Ranunculaceae,\nnative and endemic to\nwestern and central Europe.\nIt is an herbaceous perennial\nplant growing to 1 m\n(3 ft 3 in) tall, with\nhairless stems and leaves.",
     "img": "assets/RetailerFlowers/Aconite.jpg",
     "p_price": 75.00,
     "p_promotion_price": 60.00
@@ -34,7 +34,7 @@ List items = [
     "id": 4,
     "product": "Allium",
     "productdetail":
-        " Allium Contains: \n Allium is a genus of\n monocotyledonous flowering\n plants that includes hundreds of \nspecies, including the cultivated\n onion, garlic, scallion, shallot,\n leek, chives. The generic name \nAllium is the Latin word for garlic,\n and the type species for\n the genus is Allium sativum\n which means cultivated garlic",
+        "Allium Contains:\nAllium is a genus of\nmonocotyledonous flowering\nplants that includes hundreds\nof species, including the\ncultivated onion, garlic,\nscallion, shallot, leek, chives.\nThe generic name Allium is the\nLatin word for garlic,\nand the type species for\nthe genus is Allium sativum\nwhich means cultivated garlic",
     "img": "assets/RetailerFlowers/Allium.jpg",
     "p_price": 97.00,
     "p_promotion_price": 60.00
@@ -43,7 +43,7 @@ List items = [
     "id": 5,
     "product": "Alstroemeria",
     "productdetail":
-        " Alstroemeria Contains:\nAlstroemeria commonly called \nthe Peruvian lily or lily of the Incas,\n is a genus of flowering plants\n in the family Alstroemeriaceae. \nThey are all native to South America\n although some have become \nnaturalized in the United States,\n Mexico, Australia, New Zealand,\n Madeira and the Canary Islands.",
+        "Alstroemeria Contains:\nAlstroemeria commonly called\nthe Peruvian lily or lily of the\nIncas, is a genus of flowering\nplants in the family\nAlstroemeriaceae. They are all\nnative to South America\nalthough some have become\nnaturalized in the United States,\nMexico, Australia, New Zealand,\nMadeira and the Canary Islands.",
     "img": "assets/RetailerFlowers/Alstroemeria.jpg",
     "p_price": 78.00,
     "p_promotion_price": 50.00
@@ -52,7 +52,7 @@ List items = [
     "id": 6,
     "product": "Billbergia",
     "productdetail":
-        " Billbergia Contains:\n billbergia, any member of a genus\n (Billbergia) of evergreen epiphytes\n of the pineapple family\n (Bromeliaceae), containing more\n than 50 South American species. \nSeveral species are grown indoors\n as decorative plants for their\n colourful foliage, flowers, or bracts\n (leaflike clusters beneath\n the flowers).",
+        "Billbergia Contains:\nBillbergia, any member of a\ngenus (Billbergia) of\nevergreen epiphytes of the\npineapple family\n(Bromeliaceae), containing\n50 South American species.\nSeveral species are grown\nindoors as decorative\nplants for their colourful\nfoliage, flowers, or bracts\n(leaflike clusters beneath\nthe flowers).",
     "img": "assets/RetailerFlowers/Billbergia.jpg",
     "p_price": 85.00,
     "p_promotion_price": 60.00
@@ -61,7 +61,7 @@ List items = [
     "id": 7,
     "product": "Blanket",
     "productdetail":
-        " Blanket Contains:\n Blanket is a genus of flowering \nplants in the sunflower family,\n Asteraceae, native to North and\n South America. It was named\n after Maître Gaillard\n de Charentonneau, an 18th-century\n French magistrate who was\n an enthusiastic botanist",
+        "Blanket Contains:\nBlanket is a genus of flowering\nplants in the sunflower family,\nAsteraceae, native to North and\nSouth America. It was named\nafter Maître Gaillard\nde Charentonneau, an 18th\ncentury French magistrate\nwho was an enthusiastic\nbotanist",
     "img": "assets/RetailerFlowers/Blanket.jpg",
     "p_price": 89.00,
     "p_promotion_price": 40.00
@@ -70,7 +70,7 @@ List items = [
     "id": 8,
     "product": "Bletilla",
     "productdetail":
-        " Bletilla Contains:\nBletilla, common name \nurn orchid, is a temperate,\n terrestrial genus of orchids\n containing five currently recognized\n species distributed through China,\n Japan, Taiwan, south to Vietnam,\n Thailand and Myanmar.",
+        "Bletilla Contains:\nBletilla, common name \nurn orchid, is a temperate,\nterrestrial genus of orchids\ncontaining five currently\nrecognized species\ndistributed through China,\nJapan, Taiwan, south\nto Vietnam, Thailand\nand Myanmar.",
     "img": "assets/RetailerFlowers/Bletilla.jpg",
     "p_price": 99.00,
     "p_promotion_price": 70.00
@@ -79,7 +79,7 @@ List items = [
     "id": 9,
     "product": "CallaLily",
     "productdetail":
-        " CallaLily Contains:\n CallaLily is a genus of eight\n species of herbaceous, perennial,\n flowering plants in the family\n Araceae, native to southern Africa \nfrom South Africa north to Malawi.\n The genus has been introduced on\n all continents except Antarctica",
+        "CallaLily Contains:\nCallaLily is a genus of eight\nspecies of herbaceous,\nperennial, flowering plants in\nthe family Araceae, native\nto southern Africa from South\nAfrica north to Malawi. The\ngenus has been introduced on\nall continents except Antarctica",
     "img": "assets/RetailerFlowers/CallaLily.jpg",
     "p_price": 88.00,
     "p_promotion_price": 60.00
@@ -88,7 +88,7 @@ List items = [
     "id": 10,
     "product": "CannaLily",
     "productdetail":
-        " CannaLily Contains:\n Cannalily plant is a rhizomatous\n perennial with tropical-like foliage\n and large flowers that resemble\n that of iris.Canna lilies are low \n maintenance and easy to grow,\n and both their flowers and foliage \n offer long lasting color in the\n garden. The flower color may be red,\n orange, or yellow",
+        "CannaLily Contains:\nCannalily plant is a rhizomatous\nperennial with tropical-like\nfoliage and large flowers\nthat resemble that of iris.Canna\nlilies are low maintenance and\neasy to grow and both their\nflowers and foliage offer\nlong lasting color in the garden.\nThe flower color may be red,\norange, or yellow",
     "img": "assets/RetailerFlowers/CannaLily.jpg",
     "p_price": 100.00,
     "p_promotion_price": 60.00
@@ -97,7 +97,7 @@ List items = [
     "id": 11,
     "product": "Capeleadwort",
     "productdetail":
-        " Capeleadwort Contains:\n Capeleadwort is an evergreen vine \nto sprawling, mounding shrub \nboasting a profusion of beautiful\n sky-blue, five-lobed,trumpet-shaped\n flowers on short racemes\n over a long season.",
+        "Capeleadwort Contains:\nCapeleadwort is an evergreen\nvine to sprawling, mounding\nshrub boasting a profusion\nof beautiful sky-blue,\nfive-lobed,trumpet-shaped\nflowers on short racemes\nover a long season.",
     "img": "assets/RetailerFlowers/Capeleadwort.jpg",
     "p_price": 120.00,
     "p_promotion_price": 60.00
@@ -106,7 +106,7 @@ List items = [
     "id": 12,
     "product": "Cardinal",
     "productdetail":
-        " Cardinal Contains:\n Cardinal flower is a brilliant scarlet \n colored native wildflower that grows\n in marshes, stream banks and low\n woods. Its extremely showy\n blossoms can be recognized at \nconsiderable distance. Few native \nplants have flowers of such intense\n color as this common\n herbaceous perennial.",
+        "Cardinal Contains:\nCardinal flower is a brilliant\nscarlet colored native wildflower\nthat grows in marshes,\nstream banks and low woods.\nIts extremely showy\nblossoms can be recognized at\nconsiderable distance. Few\nnative plants have flowers of\nsuch intense color as this\ncommon herbaceous perennial.",
     "img": "assets/RetailerFlowers/Cardinal.jpg",
     "p_price": 98.00,
     "p_promotion_price": 50.00
@@ -115,7 +115,7 @@ List items = [
     "id": 13,
     "product": "Celosia",
     "productdetail":
-        " Celosia Contains:\n Celosia is a small genus of\n edible and ornamental plants in \nthe amaranth family, Amaranthaceae.\n The generic name is derived\n from the Ancient Greek word \n, meaning burning, and refers\n to the flame-like flower heads",
+        "Celosia Contains:\nCelosia is a small genus of\nedible and ornamental plants in\nthe amaranth family,\nAmaranthaceae. The generic\nname is derived from the\nAncient Greek word, meaning\n burning, and refers to the\nflame-like flower heads",
     "img": "assets/RetailerFlowers/Celosia.jpg",
     "p_price": 90.00,
     "p_promotion_price": 45.00
@@ -124,7 +124,7 @@ List items = [
     "id": 14,
     "product": "DianthusBarbatus",
     "productdetail":
-        " DianthusBarbatus Conains:\n Dianthus barbatus is a\n biennial or short-lived perennial\n that is often grown as an annual.\n It thrives in loamy, slightly\n alkaline soil in sun or partial \nshade in hot summer climates.\n The flowers grow in rounded,\n dense clusters.\n Colors range from white through \nintense red and purple, often bicolor. \nThere are many cultivars available,\n including double-flowered \nand dwarf forms. ",
+        "DianthusBarbatus Conains:\nDianthus barbatus is a biennial\nor short-lived perennial that\nis often grown as an annual.\nIt thrives in loamy, slightly\nalkaline soil in sun or partial\nshade in hot summer climates.\nThe flowers grow in rounded,\ndense clusters. Colors range\nfrom white through intense\nred and purple, often bicolor.\nThere are many cultivars\navailable, including double\nflowered and dwarf forms. ",
     "img": "assets/RetailerFlowers/DianthusBarbatus.jpg",
     "p_price": 110.00,
     "p_promotion_price": 60.00
@@ -133,7 +133,7 @@ List items = [
     "id": 15,
     "product": "Diascia",
     "productdetail":
-        " Diascia Contains:\n Diascia is a genus\n of around 70 species of herbaceous\n annual and perennial flowering\n plants of the family Scrophulari-\n-aceae, native to southern Africa,\n including South Africa,\n Lesotho and neighbouring areas.",
+        "Diascia Contains:\nDiascia is a genus of around 70\nspecies of herbaceous annual\nand perennial flowering plants\nof the family Scrophulari-\n-aceae, native to southern\nAfrica, including South Africa,\nLesotho and neighbouring areas.",
     "img": "assets/RetailerFlowers/Diascia.jpg",
     "p_price": 90.00,
     "p_promotion_price": 35.00
