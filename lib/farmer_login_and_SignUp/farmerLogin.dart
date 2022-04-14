a522febe3fd09aa3d672a90ea57c016890db3e25
Anshuman021 <anshumanjodha21@gmail.com>
2022-04-14 23:58:01 +0530

    Login Validation Done

--- lib/farmer_login_and_SignUp/farmerLogin.dart
+++ lib/farmer_login_and_SignUp/farmerLogin.dart
@@ -1,5 +1,8 @@
 // ignore: file_names
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 class FarmerLogin extends StatefulWidget {
   const FarmerLogin({Key? key}) : super(key: key);
@@ -9,6 +12,10 @@ class FarmerLogin extends StatefulWidget {
 }
 
 class _FarmerLoginState extends State<FarmerLogin> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -32,71 +39,107 @@ class _FarmerLoginState extends State<FarmerLogin> {
                   top: MediaQuery.of(context).size.height * 0.5,
                   right: 35,
                   left: 35),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                        fillColor: Colors.grey.shade100,
-                        filled: true,
-                        hintText: "Email",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        fillColor: Colors.grey.shade100,
-                        filled: true,
-                        hintText: "Password",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('SIGN IN',
-                          style: TextStyle(
-                              color: Colors.amberAccent,
-                              fontSize: 27,
-                              fontWeight: FontWeight.w700)),
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundColor: const Color(0xff4c505b),
-                        child: IconButton(
-                          color: Colors.amber,
-                          onPressed: () {
-                            Navigator.pushNamed(context, 'farmer_drawer_body');
-                          },
-                          icon: const Icon(Icons.arrow_forward),
-                        ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      TextButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, 'register');
-                          },
-                          child: const Text('SIGN UP',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontSize: 24,
-                                color: Colors.lightBlueAccent,
-                              )))
-                    ],
-                  )
-                ],
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      controller: _emailController,
+                      autocorrect: true,
+                      autofocus: true,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (val) {
+                        if (val!.isEmpty) {
+                          return "Please Enter The Email Address";
+                        }
+
+                        if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}')
+                            .hasMatch(val)) {
+                          return "Incorrect Email";
+                        }
+                      },
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: "Email",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      validator: (val) {
+                        if (val!.isEmpty) {
+                          return "Please Enter the Password";
+                        }
+                        if (val.length < 5) {
+                          return "Enter Atleast 5 Character";
+                        }
+                        if (val.contains("@") ||
+                            val.contains("#") ||
+                            val.contains("*")) {
+                          return null;
+                        } else {
+                          return "One Special Character Needed";
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('SIGN IN',
+                            style: TextStyle(
+                                color: Colors.amberAccent,
+                                fontSize: 27,
+                                fontWeight: FontWeight.w700)),
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: const Color(0xff4c505b),
+                          child: IconButton(
+                            color: Colors.amber,
+                            onPressed: () {
+                              if (_formkey.currentState!.validate()) {
+                                Navigator.pushNamed(
+                                    context, 'farmer_drawer_body');
+                              }
+                            },
+                            icon: const Icon(Icons.arrow_forward),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, 'register');
+                            },
+                            child: const Text('SIGN UP',
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 24,
+                                  color: Colors.lightBlueAccent,
+                                )))
+                      ],
+                    )
+                  ],
+                ),
               ),
             )
           ],
--- lib/retailer_login_signup/retailerlogin.dart
+++ lib/retailer_login_signup/retailerlogin.dart
@@ -8,6 +8,10 @@ class RetailerLogin extends StatefulWidget {
 }
 
 class _RetailerLoginState extends State<RetailerLogin> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -32,72 +36,108 @@ class _RetailerLoginState extends State<RetailerLogin> {
                   top: MediaQuery.of(context).size.height * 0.5,
                   right: 35,
                   left: 35),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                        fillColor: Colors.grey.shade100,
-                        filled: true,
-                        hintText: "Email",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        fillColor: Colors.grey.shade100,
-                        filled: true,
-                        hintText: "Password",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('SIGN IN',
-                          style: TextStyle(
-                              color: Colors.yellow,
-                              fontSize: 27,
-                              fontWeight: FontWeight.w700)),
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundColor: const Color(0xff4c505b),
-                        child: IconButton(
-                          color: Colors.yellow,
-                          onPressed: () {
-                            Navigator.pushNamed(
-                                context, 'Retailer_drawer_body');
-                          },
-                          icon: const Icon(Icons.arrow_forward),
-                        ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      TextButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, 'retailersignup');
-                          },
-                          child: const Text('SIGN UP',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontSize: 25,
-                                color: Colors.greenAccent,
-                              )))
-                    ],
-                  )
-                ],
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      controller: _emailController,
+                      autocorrect: true,
+                      autofocus: true,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (val) {
+                        if (val!.isEmpty) {
+                          return "Please Enter The Email Address";
+                        }
+
+                        if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}')
+                            .hasMatch(val)) {
+                          return "Incorrect Email";
+                        }
+                      },
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: "Email",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: "Password",
+                          helperText: "Include !@# for strong password",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      validator: (val) {
+                        if (val!.isEmpty) {
+                          return "Please Enter the Password";
+                        }
+                        if (val.length < 5) {
+                          return "Enter Atleast 5 Character";
+                        }
+                        if (val.contains("@") ||
+                            val.contains("#") ||
+                            val.contains("*")) {
+                          return null;
+                        } else {
+                          return "One Special Character Needed";
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('SIGN IN',
+                            style: TextStyle(
+                                color: Colors.yellow,
+                                fontSize: 27,
+                                fontWeight: FontWeight.w700)),
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: const Color(0xff4c505b),
+                          child: IconButton(
+                            color: Colors.yellow,
+                            onPressed: () {
+                              if (_formkey.currentState!.validate()) {
+                                Navigator.pushNamed(
+                                    context, 'Retailer_drawer_body');
+                              }
+                            },
+                            icon: const Icon(Icons.arrow_forward),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, 'retailersignup');
+                            },
+                            child: const Text('SIGN UP',
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 25,
+                                  color: Colors.greenAccent,
+                                )))
+                      ],
+                    )
+                  ],
+                ),
               ),
             )
           ],
